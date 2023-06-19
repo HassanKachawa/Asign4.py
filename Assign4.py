ded58033a42669bd9970f39d97de919544813e3b
HassanKachawa <99229716+HassanKachawa@users.noreply.github.com>
2023-06-19 07:11:56 +0530

    Add files via upload

---
+++ Assign4.py
@@ -0,0 +1,13 @@
+var=input("Enter the Varaiale:")
+value=ord(var)
+print("ASCII value of your variable is :",ord(var))
+print("Variable value of ASCII Number is :",chr(value))
+
+
+value=int(input("Enter the value of Alcohol drunked by the Driver:"))
+if (value<=40):
+    pass
+    print("The driver has less rate of Alcohol consumption")
+else:
+    print("The driver has high rate of Alcohol consumption",value)
+
